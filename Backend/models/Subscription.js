46dd440e0b07cc0cb92fe44dd8a7852ce165f5d2
vinay <girisaivinay1@gmail.com>
2025-07-26 13:43:54 +0530

    Suscription/checkout error Fix

--- Backend/models/Subscription.js
+++ Backend/models/Subscription.js
@@ -5,21 +5,3 @@ const subscriptionSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Subscription', subscriptionSchema);
-
-
-// FILE: middlewares/authMiddleware.js
-const jwt = require('jsonwebtoken');
-
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.sendStatus(401);
-
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.sendStatus(403);
-        req.user = user;
-        next();
-    });
-};
-
-module.exports = authenticateToken;
